09b219ee8905a33b668840d588dfb5d5242c4227
André Barata <andre.quina.barata@gmail.com>
2016-05-18 20:30:26 +0100

    Test usage of cash

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 var termApp = angular.module('termApp', []);
 
-termApp.factory('$childProcess', function() {
-	return require('child_process');
+termApp.factory('$ansiToHTML', function () {
+	var Convert = require('ansi-to-html');
+	var convert = new Convert();
+
+	return (ansi) => {
+		return convert.toHtml(ansi);
+	}
+});
+
+termApp.factory('$cash', function($ansiToHTML) {
+	var remote = require('electron').remote;
+
+	return (command, options) => {
+		return $ansiToHTML(
+			remote.require('cash')(command, options)
+		);
+	}
 });
 
 termApp.factory('$electron', function() {
--- js/controllers/term.ctrl.js
+++ js/controllers/term.ctrl.js
@@ -1,35 +1,16 @@
-termApp.controller("mainController", function($scope, $childProcess, $electron) {
-	var terminal = $childProcess.spawn('bash');
+termApp.controller("mainController", function($scope, $cash, $electron) {
 
 	$scope.command = "";
 	$scope.output = "";
 
-	terminal.stdout.on('data', function(m) {
-		// Receive results from child process
-		$scope.output += m;
-		$scope.$apply();
-
-		window.scrollTo(0,document.body.scrollHeight);
-	});
-
-
 	$scope.execute = (keyEvent) => {
 		if (keyEvent.which === 13) {
-			terminal.stdin.write(`${$scope.command}\n`);
-
 			$scope.output += `${$scope.command}\n\n`;
+			$scope.output += $cash($scope.command);
 
 			$scope.command = "";
+
+			window.scrollTo(0,document.body.scrollHeight);
 		}
 	}
-
-	$electron.webFrame.on("close",function() {
-			terminal.close();
-		}
-	);
-
-	terminal.on('close', function () {
-			$electron.remote.app.quit();
-		}
-	);
 });
